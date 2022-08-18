db40f9de25237c8905ca973cc601fad6652ef96b
mikeyrf <23126274+mikeyrf@users.noreply.github.com>
2022-08-18 18:16:54 -0400

    Cleanup prints

--- pystable/utils.py
+++ pystable/utils.py
@@ -7,11 +7,6 @@ PATH_DEFAULT = '{}libstable.so'.format(BASE)
 def libstable_path(libstable_path=None) -> str:
     '''Get path to libstable.so'''
     libstable_path = PATH_DEFAULT
-    # TODO: remove
-    print('libstable_path', libstable_path)
     path = os.path.dirname(os.path.realpath(__file__))
     path = os.path.abspath(os.path.join(path, os.pardir))
-    # TODO: remove
-    print('os.path.abspath(os.path.join(path, libstable_path))',
-          os.path.abspath(os.path.join(path, libstable_path)))
     return os.path.abspath(os.path.join(path, libstable_path))
